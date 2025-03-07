18393ccf72d74b2dedd95270038ebba0e07be71d
HarunOktayB <harunoktayb@gmail.com>
2025-03-07 21:29:13 +0300

    added views

--- Starter Code/api/urls.py
+++ Starter Code/api/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('products/', views.product_list, name='product-list'),
+    # path('products/', views.product_list, name='product-list'),
+    path('products/', views.ProductListAPIView.as_view(), name='product-list'),
+    path('products/stock', views.StockProductListAPIView.as_view(), name='stock-products'),
+    path('products/out-of-stock', views.nonStockProductListAPIView.as_view(), name='out-of-stock-products'),
+    path('orders/', views.OrderListAPIView.as_view(), name='order-list'),
+    
     path('products/info/', views.product_info, name='product-info'),    
-    path('products/<int:pk>/', views.product_detail, name='product-detail'),
-     path('orders/', views.order_list, name='order-list'),
+    path('products/<int:pk>/', views.ProductDetailAPIView.as_view(), name='product-detail'),
 ]
--- Starter Code/api/views.py
+++ Starter Code/api/views.py
@@ -4,30 +4,62 @@ from . models import Product, Order, OrderItem
 from .serializers import ProductSerializer, OrderSerializer, ProductInfoSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import generics
 
-@api_view(['GET'])
-def product_list(request):
-    products = Product.objects.all()
-    print(type(products)) 
-    serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data)
+# @api_view(['GET'])
+# def product_list(request):
+#     products = Product.objects.all()
+#     print(type(products)) 
+#     serializer = ProductSerializer(products, many=True)
+#     return Response(serializer.data)
 
-@api_view(['GET'])
-def product_detail(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    print(type(product))  
-    serializer = ProductSerializer(product)
-    return Response(serializer.data)
+class ProductListAPIView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
 
-@api_view(['GET'])
-def order_list(request):
-    orders = Order.objects.prefetch_related(
+class StockProductListAPIView(generics.ListAPIView):
+    queryset = Product.objects.all().filter(stock__gt = 0)
+    serializer_class = ProductSerializer
+
+class nonStockProductListAPIView(generics.ListAPIView):
+    queryset = Product.objects.all().exclude(stock__gt = 0)
+    serializer_class = ProductSerializer
+
+# @api_view(['GET'])
+# def product_detail(request, pk):
+#     product = get_object_or_404(Product, pk=pk)
+#     print(type(product))  
+#     serializer = ProductSerializer(product)
+#     return Response(serializer.data)
+
+class ProductDetailAPIView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+# @api_view(['GET'])
+# def order_list(request):
+#     orders = Order.objects.prefetch_related(
+#         'items',
+#         'items__product',
+#     ).all()
+#     print(type(orders))  
+#     serializer = OrderSerializer(orders, many=True)
+#     return Response(serializer.data)
+
+class OrderListAPIView(generics.ListAPIView):
+    queryset = Order.objects.prefetch_related(
         'items',
         'items__product',
     ).all()
-    print(type(orders))  
-    serializer = OrderSerializer(orders, many=True)
-    return Response(serializer.data)
+    serializer_class = OrderSerializer
+
+class UserOrderListAPIView(generics.ListAPIView):
+    queryset = Order.objects.prefetch_related(
+        'items__product',
+    )
+    serializer_class = OrderSerializer
+    def get_queryset(self):
+        return self.get_queryset().filter(user_id=self.request.user.id)
 
 @api_view(['GET'])
 def product_info(request):
@@ -37,6 +69,5 @@ def product_info(request):
         'count': len(products),
         'max_price' : products.aggregate(max_price = Max('price'))['max_price']
     })
-
     return Response(serializer.data)
     
